7d5a6d86a25fb7583f16161dd44402e20af1a66b
Gilbert Torchon <gilbert@noukod.com>
2019-03-21 15:09:46 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  state={
+    people:[]
+  }
+
+  componentDidMount(){
+    fetch("/http://api.open-notify.org/astros.json")
+    .then((res)=>res.json())
+    .then((jsonRes)={
+      this.setState({
+        people: this.state.people
+      })
+    })
+  }
+  
   render(){
     return(
-      <div>Hello App</div>
+      {this.state.people.map((people)=>{
+        return(
+          <div>
+            <h4>{ people.name }</h4>
+            <h4>{ people.craft }</h4>
+          </div>
+        )
+      })}
     )
   }
 }
